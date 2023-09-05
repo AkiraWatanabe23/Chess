d1b5ad6149c102943a5bfac28787242bcea63421
AkiraWatanabe23 <a0323w@gmail.com>
2023-09-05 23:41:17 +0900

    [add] Debug用クラス

---
+++ Assets/Scripts/System/Others/Debugger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Debugger : SingletonMonoBehaviour<Debugger>
+{
+    [SerializeField]
+    private bool _debugMode = false;
+
+    public bool DebugMode => _debugMode;
+
+    protected override bool DontDestroyOnLoad => true;
+}
---
+++ Assets/Scripts/System/Singleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public abstract class Singleton<T>
+{
+    public static T Instance { get; private set; }
+
+    public Singleton()
+    {
+        Instance ??= (T)Activator.CreateInstance(typeof(T));
+    }
+}
